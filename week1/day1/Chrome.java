bccb3ed4acf108503de5122746f8ec2b2f5e7a9a
Likhith R <78309314+likhithr04@users.noreply.github.com>
2025-07-19 15:26:22 +0530

    Add files via upload
    week1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. version of type float with a value of 91.0f
+		float version = 91.0f;
+		System.out.println(version);
+		
+		// 2. developer of type String with a value of "Google"
+		String developer = "Google";
+		System.out.println("Android is developed by: " + developer);
+		
+		// 3. isBeta of type boolean with a value of false
+		boolean isBeta = false;
+		System.out.println("value of isBeta = " + isBeta);
+
+		// 4. releaseYear of type int with a value of 2008
+		int releaseYear = 2008;
+		System.out.println("Ghajini was released in the year = " + releaseYear);
+
+		// 5. shortcutKey of type char with a value of 'C'
+		char shortcutKey = 'C';
+		System.out.println("shortcutKey of copy = " + shortcutKey);
+	}
+
+}
