26e489ca935f7fd33ee52bc1736b046063708b0e
Likhith R <78309314+likhithr04@users.noreply.github.com>
2025-07-26 16:29:58 +0530

    Add files via upload
    week2day1

---
+++ week2/day1/Browser.java
@@ -0,0 +1,24 @@
+package week2.day1;
+
+public class Browser {
+	
+	public String launchBrowser(String browserName)
+	{
+		System.out.println(browserName+ "Browser launched successfully");
+		return browserName;
+	}
+	
+	public void loadUrl()
+	{
+		System.out.println("Application url loaded successfully");
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Browser b =new Browser();
+       b.launchBrowser("Chrome");
+       b.loadUrl();
+	}
+
+}
---
+++ week2/day1/LibraryManagement.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		Library lb = new Library();
+		lb.addBook("The Lord of the Rings ");
+		lb.issueBook();
+		
+	}
+
+}
